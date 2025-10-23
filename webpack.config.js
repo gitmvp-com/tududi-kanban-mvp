f79544ba64ee3bdc38bf39853e82224876a9ed89
Fili <126945152+filiksyos@users.noreply.github.com>
2025-10-23 16:17:49 +0300

    Initial commit: MVP with task statuses and kanban view

---
+++ webpack.config.js
@@ -0,0 +1,95 @@
+const path = require('path');
+const webpack = require('webpack');
+const ReactRefreshWebpackPlugin = require('@pmmmwh/react-refresh-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+const isDevelopment = process.env.NODE_ENV !== 'production';
+
+module.exports = {
+  entry: './frontend/index.tsx',
+  cache: {
+    type: 'filesystem',
+    buildDependencies: {
+      config: [__filename],
+    },
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[contenthash].js',
+    publicPath: '/',
+    clean: true
+  },
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+  },
+  devServer: {
+    static: {
+      directory: path.join(__dirname, 'public'),
+    },
+    hot: isDevelopment,
+    watchFiles: isDevelopment ? ['frontend/**/*'] : [],
+    port: 8080,
+    host: '0.0.0.0',
+    historyApiFallback: true,
+    proxy: [{
+      context: ['/api'],
+      target: 'http://localhost:3002',
+      changeOrigin: true,
+      secure: false,
+      cookieDomainRewrite: 'localhost',
+      headers: {
+        'Access-Control-Allow-Origin': '*'
+      },
+      onProxyRes: function(proxyRes, req, res) {
+        proxyRes.headers['Access-Control-Allow-Origin'] = 'http://localhost:8080';
+        proxyRes.headers['Access-Control-Allow-Credentials'] = 'true';
+      }
+    }],
+  },
+  plugins: [
+    isDevelopment && new ReactRefreshWebpackPlugin(),
+    isDevelopment && new webpack.HotModuleReplacementPlugin(),
+    new HtmlWebpackPlugin({
+      title: 'Tududi Kanban MVP',
+      filename: 'index.html',
+      template: 'public/index.html'
+    }),
+  ].filter(Boolean),
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx|ts|tsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'swc-loader',
+          options: {
+            jsc: {
+              parser: {
+                syntax: 'typescript',
+                tsx: true,
+                decorators: false,
+                dynamicImport: true,
+              },
+              transform: {
+                react: {
+                  runtime: 'automatic',
+                  development: isDevelopment,
+                  refresh: isDevelopment,
+                },
+              },
+              target: 'es2018',
+            },
+            module: {
+              type: 'es6',
+            },
+          },
+        },
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader', 'postcss-loader'],
+      },
+    ],
+  },
+  mode: isDevelopment ? 'development' : 'production',
+};
