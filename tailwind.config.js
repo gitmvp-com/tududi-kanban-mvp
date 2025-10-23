2ae9776edfe7d9c06e731f6bd8d6f3b710df75fe
Fili <126945152+filiksyos@users.noreply.github.com>
2025-10-23 16:17:49 +0300

    Initial commit: MVP with task statuses and kanban view

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  darkMode: 'class',
+  content: [
+    './frontend/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
